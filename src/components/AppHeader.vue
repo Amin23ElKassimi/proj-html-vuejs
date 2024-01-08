b35e8b6d2b384949ffca38f2fe4f9014ca69a9f6
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-01-08 16:47:38 +0100

    header - Nav List - Size Img Fixeed - Display Flex Nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,10 @@
 <template lang="">
     <header>
         <div class="header-back">
-            <nav >
+            <nav>
+                <div>
+                    <img src="../assets/img/author-logo-round-small.png" alt="">
+                </div>
                 <ul class="m-0">
                     <li v-for="(link, index) in navLinks" >
                     {{ link.content}}
@@ -24,52 +27,42 @@ export default {
         return {
             navLinks: [
                 {
-                    content: 'charters',
+                    content: 'Home',
                     url: '#',
                     active: false,
                 },
                 {
-                    content: 'comics',
+                    content: 'About Me',
                     url: '#',
                     active: false,
                 },
                 {
-                    content: 'movies',
+                    content: 'Testimonials',
                     url: '#',
                     active: false,
                 },
                 {
-                    content: 'tv',
+                    content: 'My Blog',
                     url: '#',
                     active: false,
                 },
                 {
-                    content: 'games',
-                    url: '#',
-                    active: false,
-                },
-                {
-                    content: 'collectibles',
-                    url: '#',
-                    active: false,
-                },
-                {
-                    content: 'videos',
-                    url: '#',
-                    active: false,
-                },
-                {
-                    content: 'fans',
-                    url: '#',
-                    active: false,
-                },
-                {
-                    content: 'news',
+                    content: 'Meetsup',
                     url: '#',
                     active: false,
                 },
                 {
                     content: 'shop',
+                    url: '#',
+                    active: false,
+                },
+                {
+                    content: 'Contact Me',
+                    url: '#',
+                    active: false,
+                },
+                {
+                    content: 'Carrello',
                     url: '#',
                     active: false,
                 },
@@ -89,25 +82,37 @@ export default {
   @use '../styles/general.scss' as *;
   @use '../styles/partials/mixins' as *;
 
-
-
-  .header-back{
-    background-image: url(../assets/img/footer-3.jpg);
-    object-fit: contain;
-    height: 700px;
-    width: 100%;
-  }
-
-  ul {
-        @include flex(row,space-between,center);
-        list-style-type:none ;
+    header {
+        width: 100vw;
+        height: 800px;
     }
 
+    .header-back{
+        background: url(../assets/img/footer-3.jpg) no-repeat center center;
+        margin: 0;
+        padding: 0;
+        background-size: cover;
+        width: 100%;
+        height: 500px;
+    }
+
+    nav {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    ul {
+            @include flex(row,space-between,center);
+            list-style-type:none ;
+            color: white;
+        }
+
     li{
-        font-size: 1.2rem;
+        font-size: 0.8rem;
         padding: 3rem 1rem;
         font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
         text-transform: uppercase;
+        height: 100%;
         &:hover{
             color: #f4b429;
             border-top: 5px solid #f4b429;

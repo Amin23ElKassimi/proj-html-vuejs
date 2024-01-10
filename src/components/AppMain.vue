f7081cdd7c92ca854411aa901ec4e8ddd09c3440
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-01-10 12:51:33 +0100

    Separate in sections

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,116 +1,128 @@
 <!-- HTML & Vue JS-->
 <template lang="">
     <main>
-              <!-- Section 01: Audio Book-->
-        <!-- Where Listen AudioBook  -->
-        <div class="audio-book d-flex justify-content-center">
-            <div class="pubblication">
-              <img src="../assets/img/ebay-logotan-2x.png" alt="">
-            </div>
-            <div class="pubblication">
-              <img src="../assets/img/audible-logotan-2x.png" alt="">
-            </div>
-            <div class="pubblication">
-              <img src="../assets/img/bb-logotan-2x.png" alt="">
-            </div>
-            <div class="pubblication">
-              <img src="../assets/img/kindlefire-logotan-2x.png" alt="">
-            </div>
-        </div>
-        <!-- Detail -->
-        <div id="audio-detail">
-          <div class="wrapper-sec01">
-            <!-- Image home -->
-            <div class="photoaudio cardaudio">
-              <img src="../assets/img/audible-app-2x.png" alt="">
-            </div>
-            <!-- Detail AudioBook -->
-            <div class="cardaudio text-light mb-5">
-              <h3>
-                New AudioBook
-              </h3>
-              <p class="danger-color">
-                no time to read? no problem. listen to it
-              </p>
-              <p class="text-gray">
-                Et quibusdam quo ipsa id soluta recusandae dicta ipsum quas eum deserunt iure dolores iusto ad reiciendis, necessitatibus consectetur explicabo tenetur unde!
-              </p>
-              <div class="size-img">
-                <img src="../assets/img/audible-logo.png" alt="">
-                <img src="../assets/img/bn-logo-tall.png" alt="">
-                <img src="../assets/img/kindle-logo-tall.png" alt="">
+        <!-- Section 01: Audio Book-->
+        <section>
+          <!-- Where Listen AudioBook  -->
+          <div class="audio-book d-flex justify-content-center">
+              <div class="pubblication">
+                <img src="../assets/img/ebay-logotan-2x.png" alt="">
+              </div>
+              <div class="pubblication">
+                <img src="../assets/img/audible-logotan-2x.png" alt="">
+              </div>
+              <div class="pubblication">
+                <img src="../assets/img/bb-logotan-2x.png" alt="">
+              </div>
+              <div class="pubblication">
+                <img src="../assets/img/kindlefire-logotan-2x.png" alt="">
+              </div>
+          </div>
+          <!-- Detail -->
+          <div id="audio-detail">
+            <div class="wrapper-sec01">
+              <!-- Image home -->
+              <div class="photoaudio cardaudio">
+                <img src="../assets/img/audible-app-2x.png" alt="">
+              </div>
+              <!-- Detail AudioBook -->
+              <div class="cardaudio text-light mb-5">
+                <h3>
+                  New AudioBook
+                </h3>
+                <p class="danger-color">
+                  no time to read? no problem. listen to it
+                </p>
+                <p class="text-gray">
+                  Et quibusdam quo ipsa id soluta recusandae dicta ipsum quas eum deserunt iure dolores iusto ad reiciendis, necessitatibus consectetur explicabo tenetur unde!
+                </p>
+                <div class="size-img">
+                  <img src="../assets/img/audible-logo.png" alt="">
+                  <img src="../assets/img/bn-logo-tall.png" alt="">
+                  <img src="../assets/img/kindle-logo-tall.png" alt="">
+                </div>
               </div>
             </div>
           </div>
-        </div>
-              <!-- Section 02: What The Critics Say-->
+        </section>
+
+        <!-- Section 02: What The Critics Say-->
         <setion class="critics">
             <div class="wrapper-sec02" >
-              <!-- What the critics Say -->
-              <div class="what-critics">
-                <h1>
-                  What The Critics Say
-                </h1>
-                <p class="text-success text-uppercase">
-                  tesimonials from around the world
-                </p>
-                <!-- Background img -->
-                <div class="text-light">
-                  <h2>
-                    Best Author Of His Generation
-                  </h2>
-                  <hr>
-                  <p>
-                    This Book Will change your perspective on life
-                  </p>
-                  <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dicta aliquam corporis officia modi libero minima hic iste eveniet tenetur! Accusantium quae quidem velit labore provident reiciendis eveniet dolor maiores.
-                  </div>
-                  <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dicta aliquam corporis officia modi libero minima hic iste eveniet tenetur! Accusantium quae quidem velit labore provident reiciendis eveniet dolor maiores.
-                  </div>
-                  <p class="mt-5">
-                    <img src="../assets/img/nytimes-logo-white-xsmall.png" alt="">
-                    Justine Kilpatrick, 
-                    <span class="fw-bold text-success mt-5">The New York Times</span>
+              <!-- Title -->
+              <div class="d-flex justify-content-between">
+                <div>
+                  <h1>
+                    What The Critics Say
+                  </h1>
+                  <p class="text-success text-uppercase">
+                    tesimonials from around the world
                   </p>
                 </div>
+                <button id="readTestimonials" type="button" class="btn btn-warning text-light fw-bold">Read All Testiomonials</button>
               </div>
-              <!-- Testimonials -->
-              <div class="testimonials px-4">
-                <button type="button" class="btn btn-warning text-light fw-bold">Read All Testiomonials</button>
-                <div class="mb-3">
-                  <h3 class="text-capitalize">a ture masterpiece, bravo!</h3>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi earum pariatur ut perferendis non quibusdam maiores ex. Porro veritatis facilis numquam, ut facere pariatur harum, et nisi corrupti quis est.
-                </div>
-                <p class="mb-5">
-                  <img src="../assets/img/theguardian-xsmall.png" alt="">
-                  Gerald Hendley, The Guardian
-                </p>
-                <div class="mb-3">
-                  <h3 class="text-capitalize">a ture masterpiece, bravo!</h3>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi earum pariatur ut perferendis non quibusdam maiores ex. Porro veritatis facilis numquam, ut facere pariatur harum, et nisi corrupti quis est.
-                </div>
-                <p class="mb-5">
-                  <img src="../assets/img/globe-xsmall.png" alt="">
-                  Mary Maxey, The Globe And Mail
-                </p>
+              <!-- Main Section -->
+              <div class="d-flex">
+                  <!-- What the critics Say -->
+                  <div class="what-critics">
+                    <!-- Background img -->
+                    <div class="text-light">
+                      <h2>
+                        Best Author Of His Generation
+                      </h2>
+                      <hr>
+                      <p>
+                        This Book Will change your perspective on life
+                      </p>
+                      <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dicta aliquam corporis officia modi libero minima hic iste eveniet tenetur! Accusantium quae quidem velit labore provident reiciendis eveniet dolor maiores.
+                      </div>
+                      <div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dicta aliquam corporis officia modi libero minima hic iste eveniet tenetur! Accusantium quae quidem velit labore provident reiciendis eveniet dolor maiores.
+                      </div>
+                      <p class="mt-5">
+                        <img src="../assets/img/nytimes-logo-white-xsmall.png" alt="">
+                        Justine Kilpatrick, 
+                        <span class="fw-bold text-success mt-5">The New York Times</span>
+                      </p>
+                    </div>
+                  </div>
+                  <!-- Testimonials -->
+                  <div class="testimonials px-4">
+                    <div class="mb-3 p-2 beji-bg">
+                      <h3 class="text-capitalize">a ture masterpiece, bravo!</h3>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi earum pariatur ut perferendis non quibusdam maiores ex. Porro veritatis facilis numquam, ut facere pariatur harum, et nisi corrupti quis est.
+                    </div>
+                    <p class="mb-5">
+                      <img src="../assets/img/theguardian-xsmall.png" alt="">
+                      Gerald Hendley, The Guardian
+                    </p>
+                    <div class="mb-3 p-2 beji-bg">
+                      <h3 class="text-capitalize">a ture masterpiece, bravo!</h3>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi earum pariatur ut perferendis non quibusdam maiores ex. Porro veritatis facilis numquam, ut facere pariatur harum, et nisi corrupti quis est.
+                    </div>
+                    <p class="mb-5">
+                      <img src="../assets/img/globe-xsmall.png" alt="">
+                      Mary Maxey, The Globe And Mail
+                    </p>
+                  </div>
               </div>
             </div>
         </setion>
-              <!-- Section 03: Upcomin Events-->
-        <setion class="upcoming-events">
+        <!-- Section 03: Upcomin Events-->
+        <section03/>
 
-        </setion>
-              <!-- Section 04: Subscribe To My Newsletter-->
+        <!-- Section 04: Subscribe To My Newsletter-->
         <setion class="newsletter">
 
         </setion>
-              <!-- Section 05: Dig A Little Deeper Into Damon Vaughn -->
+
+        <!-- Section 05: Dig A Little Deeper Into Damon Vaughn -->
         <setion class="nav-realese">
 
         </setion>
-              <!-- Section 06: Resent Articles-->
+
+        <!-- Section 06: Resent Articles-->
         <setion class="recent-articles">
 
         </setion>
@@ -119,8 +131,18 @@
 
 <!-- Script Code -->
 <script>
+  // Impor Components
+  import section03 from './section03.vue';
+
 export default {
-    
+
+
+
+  components: {
+    section03,
+  }
+
+
 }
 </script>
 
@@ -178,44 +200,47 @@ export default {
 
     //  Section 02: What The Critics Say
 
-    .wrapper-sec02{
+  .wrapper-sec02{
+    width: 70%;
+    margin: 0 auto;
+    padding: 2rem;
+    border-radius: 20px; 
+
+  }
+  #readTestimonials{
+      height: 50px;
+    }
+
+
+  .what-critics{
+    width: 60%;
+  }
+
+  .what-critics > div{
+      background-image: url(../assets/img/testimonial2-2x.jpg);
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+      height: 585px;
+      border-radius: 0 0 100px 0px;
+      width: 100%;
+      padding: 3rem;
+    }
+
+    .testimonials{
+      width: 40%;
       display: flex;
-      align-items: end;
-      width: 70%;
-      margin: 0 auto;
-      padding: 2rem;
-      border-radius: 20px; 
+      flex-direction: column;
     }
 
+    // <!-- Section 03: Upcomin Events-->
 
 
-    .what-critics{
-      width: 60%;
-    }
-
-    .what-critics > div{
-        background-image: url(../assets/img/testimonial2-2x.jpg);
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-        height: 585px;
-        border-radius: 0 0 100px 0px;
-        width: 100%;
-        padding: 3rem;
-      }
-
-      .testimonials{
-        width: 40%;
-        display: flex;
-        flex-direction: column;
-        button{
-          align-self: flex-end;
-          width: 200px;
-          margin-bottom: 4rem;
-        }
-      }
-
-
-
+  .wrapper-sec03{
+    width: 70%;
+    margin: 0 auto;
+    padding: 2rem;
+    border-radius: 20px; 
+  }
 
     // rooles
     .text-uppercase{
@@ -230,6 +255,11 @@ export default {
 
     .text-gray{
       color: rgb(150, 147, 147);
+    }
+
+    
+    .beji-bg{
+      background-color:$beji-bg;
     }
 
     // Margin Padding Border
---
+++ src/components/section03.vue
@@ -0,0 +1,89 @@
+<!-- HTML & Vue JS-->
+<template lang="">
+    <div class="upcoming-events beji-bg">
+        <!-- Content -->
+        <div class="wrapper-sec03">
+        <!-- Title -->
+        <div class="text-center">
+            <h1 tex>Upcoming Events</h1>
+            <p>meet the author in person</p>
+        </div>
+        <!-- Cards -->
+        <div class="d-flex">
+            <!-- Card01 -->
+            <div class="card mx-4" style="width: 18rem;">
+            <img src="../assets/img/event-05.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h3>chicago book singing</h3>
+                <hr>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+            <!-- Card02 -->
+            <div class="card mx-4" style="width: 18rem;">
+            <img src="../assets/img/event-04.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h3>meet @ greet with amanda</h3>
+                <hr>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+            <!-- Card03 -->
+            <div class="card mx-4" style="width: 18rem;">
+            <img src="../assets/img/event-07.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h3>open bool: dialouges</h3>
+                <hr>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<!-- Script Code -->
+<script>
+export default {
+    
+}
+</script>
+
+
+<!-- SASS Code -->
+<style lang="scss" scoped>
+  @use '../styles/general.scss' as *;
+  @use '../styles/partials/variables.scss' as *;
+  @use '../styles/partials/mixins.scss' as *;
+
+  .wrapper-sec03{
+    width: 70%;
+    margin: 0 auto;
+    padding: 2rem;
+    border-radius: 20px; 
+  }
+
+    // rooles
+    .text-uppercase{
+      text-transform: uppercase;
+    }
+
+
+    .danger-color{
+        color: $danger-color;
+        text-transform: uppercase;
+    }
+
+    .text-gray{
+      color: rgb(150, 147, 147);
+    }
+
+    
+    .beji-bg{
+      background-color:$beji-bg;
+    }
+
+
+
+    
+</style>
